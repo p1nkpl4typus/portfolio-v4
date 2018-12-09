f1d7f7d53ede32c4436b88d77e76e01053c4b9d8
Shelagh <34905807+p1nkpl4typus@users.noreply.github.com>
2018-12-08 21:53:23 -0700

    added contact form and created php file

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+
+$data = $_POST;
+dd($data);
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "iam@shelaghstorla.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+
+
+
+
+?>
