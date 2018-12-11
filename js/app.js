9efba0d9e40ba7777dd3bf17e13a226271bccb7e
Shelagh <34905807+p1nkpl4typus@users.noreply.github.com>
2018-12-11 15:43:43 -0700

    more work on trying to get contact form to work

--- js/app.js
+++ js/app.js
@@ -1,45 +1,14 @@
-if (document.forms[0] && window.FormData) { ... }
-
-	var message = new Object();
-	message.loading = 'Loading...';
-	message.success = 'Thank you. Application received!';
-	message.failure = 'Whoops! There was a problem sending your message.';
-
-	var form = document.forms[0];
-
-	var statusMessage = document.createElement('div');
-	statusMessage.className = 'status';
-
-	// Set up the AJAX request
-	var request = new XMLHttpRequest();
-	request.open('POST', 'http://localhost/portfolio/contact.php', true);
-	request.setRequestHeader('accept', 'application/json');
-
-	// Listen for the form being submitted
-	form.addEventListener('submit', function(evt) {
-	    evt.preventDefault();
-	    form.appendChild(statusMessage);
-
-	    // Create a new FormData object passing in the form's key value pairs (that was easy!)
-	    var formData = new FormData(form);
-
-	    // Send the formData
-	    request.send(formData);
-
-	    // Watch for changes to request.readyState and update the statusMessage accordingly
-	    request.onreadystatechange = function () {
-	        // <4 =  waiting on response from server
-	        if (request.readyState < 4)
-	            statusMessage.innerHTML = message.loading;
-	        // 4 = Response from server has been completely loaded.
-	        else if (request.readyState === 4) {
-	            // 200 - 299 = successful
-	            if (request.status == 200 && request.status < 300)
-	                statusMessage.innerHTML = message.success;
-	            else
-	                form.insertAdjacentHTML('beforeend', message.failure);
-	        }
-	    }
-	});
-
-}
+const form = document.querySelector("#contact-form");
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const fd = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", event => {
+        console.log(event.target.responseText);
+    });
+    xhr.addEventListener("error", event => {
+        console.log(event);
+    });
+    xhr.open("POST", form.action);
+    xhr.send(fd);
+});
