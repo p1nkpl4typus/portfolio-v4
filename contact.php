93504182770e2e75591161a20ed040abd5562a86
Shelagh <34905807+p1nkpl4typus@users.noreply.github.com>
2018-12-11 20:26:46 -0700

    developed project cards for my work section

--- contact.php
+++ contact.php
@@ -1,17 +1,16 @@
 <?php
 
 $data = $_POST;
-var_dump($data);
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+$name = $data['name'];
+$email = $data['email'];
+$message = $data['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "iam@shelaghstorla.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error! $recipient, $subject, $formcontent, $mailheader");
+echo "Thank you, ".$name."! Your message has been sent!";
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 const form = document.querySelector("#contact-form");
+const response = document.querySelector(".response-box");
+const formBox = document.querySelector(".contact-form-box");
 form.addEventListener("submit", event => {
     event.preventDefault();
     const fd = new FormData(form);
     const xhr = new XMLHttpRequest();
     xhr.addEventListener("load", event => {
+        form.classList.add("hide");
+        response.innerHTML = "<p>" + event.target.responseText + "</p>";
+		response.classList.replace("no-display", "appear");
         console.log(event.target.responseText);
     });
     xhr.addEventListener("error", event => {
